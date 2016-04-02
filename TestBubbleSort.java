713bcb53c0d5dac25fd1a6af70158902866cf2af
WenChi <u10416019@go.utaipei.edu.tw>
2016-04-03 00:11:09 +0800

    Prototype of the bubble sort test class
    This file is create a scanner, and let the user enter how many numbers does the user want to input, then let the value to be the length of the array.

---
+++ TestBubbleSort.java
@@ -0,0 +1,17 @@
+//U10416019鄭文琪
+
+import java.util.Scanner;
+
+public class TestBubbleSort{
+	public static void main(String[] args){
+		//create a scanner
+		Scanner input = new Scanner(System.in);
+		
+		//let the user enter how many numbers want to enter
+		System.out.print("How many numbers do you want to enter ?");
+		int number = input.nextInt();
+		
+		//let the number equal the length of the bubble array
+		int[] bubble = new int[number];
+
+}
