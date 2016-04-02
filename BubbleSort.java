4595a3abcb812109a485ce42d5ce7d44adadcee5
WenChi <u10416019@go.utaipei.edu.tw>
2016-04-02 23:52:41 +0800

    Create a method to sort the numbers
    I create a method to sort the numbers which the user enter, in this method, I determine whether this number is large than the next one, if the number is large than the next one, then change the number.

--- BubbleSort.java
+++ BubbleSort.java
@@ -14,5 +14,18 @@ public class BubbleSort{
 		setPrintBubble();
 		System.out.println("");
 	}
+	
+	//create a mutator method to let the numbers which the user enter sort from small to large
+	public void setBubbleSort(){
+		for(int i = 1; i < bubble.length; i++){
+			for(int j = 0; j < bubble.length - i; j++){
+				if(bubble[j] > bubble[j + 1]){
+					keep = bubble[j];
+					bubble[j] = bubble[j + 1];
+					bubble[j + 1] = keep;
+				}
+			}
+		}
+	}
 
 }
